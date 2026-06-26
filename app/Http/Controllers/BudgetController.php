db7fe2018a2de6cf3aae9d5b8290507fe30fab8d
olmosdev <olmos.develop@gmail.com>
2026-06-26 11:32:33 -0600

    add component to display user budgets on the Dashboard page

--- app/Http/Controllers/BudgetController.php
+++ app/Http/Controllers/BudgetController.php
@@ -17,7 +17,8 @@ class BudgetController extends Controller
      */
     public function index()
     {
-        return view("dashboard");
+        $budgets = Auth::user()->budgets()->get();
+        return view("dashboard", ["budgets" => $budgets]);
     }
 
     /**
--- app/Models/Budget.php
+++ app/Models/Budget.php
@@ -2,15 +2,30 @@
 
 namespace App\Models;
 
+use App\BudgetType;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Model;
 
 #[Fillable(["name", "amount", "type", "user_id"])]
 class Budget extends Model
 {
+    protected $casts = [
+        "type" => BudgetType::class,
+    ];
+
     // Establishing an inverse relationship of User 1:N Budgets
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function isGeneral() : bool
+    {
+        return $this->type === BudgetType::General;
+    }
+
+    public function isGoal() : bool
+    {
+        return $this->type === BudgetType::Goal;
     }
 }
